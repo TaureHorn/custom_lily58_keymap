4494330d9480eb544acaa829bf39f0f77e458e14
silence <6367950+TaureHorn@users.noreply.github.com>
2024-08-20 11:27:46 +0100

    update

--- keymap.c
+++ keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
  * |LCTL  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RCTL|
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| LGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN,  KC_RPRN,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RCTL
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_LGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F21 |      |  UP  |      | vol- | vol+ |                    |  +=  |  6   |  8   |  9   |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | caps | left | down | right| play |      |-------.    ,-------|   -  |  4   |  5   |  6   |      |  "   |
+ * |      | left | down | right| play |      |-------.    ,-------|   -  |  4   |  5   |  6   |      |  "   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      | undo | del  | copy | paste| bksp |-------|    |-------|   0  |  1   |  2   |  3   | pipe |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6   ,                  KC_F7   , KC_F8   , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-  KC_F21 , _______, KC_UP  , _______, KC_VOLD , KC_VOLU ,              KC_EQL  , KC_7    , KC_8   , KC_9   , _______, _______,
-  _______, KC_LEFT, KC_DOWN, KC_RGHT,KC_MPLY, _______,                   KC_MINS , KC_4    , KC_5   , KC_6   , _______, LSFT(KC_2),
+  KC_F21 , _______, KC_UP  , _______, KC_VOLD , KC_VOLU,                  KC_EQL  , KC_7    , KC_8   , KC_9   , _______, _______,
+  _______, KC_LEFT, KC_DOWN, KC_RGHT,KC_MPLY, _______,                    KC_MINS , KC_4    , KC_5   , KC_6   , _______, LSFT(KC_2),
   _______, KC_UNDO, KC_DEL, KC_COPY, KC_PSTE, KC_BSPC, KC_LBRC,  KC_RBRC, KC_0    , KC_1    , KC_2   , KC_3   , KC_PIPE, _______, 
                              _______, _______, _______, KC_ENT, _______,  _______, _______, KC_DOT 
 ),
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |  up  |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | caps | left | down | right| home | end  |-------.    ,-------| left | down |  up  | right|      | grave|
+ * |      | left | down | right| home | end  |-------.    ,-------| left | down |  up  | right|      | grave|
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
  * |      | tilde|  *   |  #   |  -   |  _   |-------|    |-------|   _  |   -  |  #   |  *   |  \   |      |    
  * `-----------------------------------------/       /     \      \-----------------------------------------'
