e751148658471dcd48d28b4d29f7e326721da172
silence <6367950+TaureHorn@users.noreply.github.com>
2024-09-28 10:01:16 +0100

    more bracket keys

--- keymap.c
+++ keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |  up  |      |      |      |                    |  F15 | F16  | F17  | F18  | F19  | F20  |
+ * |      |      |  up  | [ {  | ] }  |      |                    |  F15 | F16  | F17  | F18  | F19  | F20  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      | left | down | right| home | end  |-------.    ,-------| left | down |  up  | right|      | grave|
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   KC_F1   , KC_F2   , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                   KC_F7   , KC_F8   , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-  _______ , _______ , KC_UP  , _______, _______, _______,                   KC_F15  , KC_F16  , KC_F17 , KC_F18 , KC_F19 , KC_F20 ,
+  _______ , _______ , KC_UP  , KC_LBRC, KC_RBRC, _______,                   KC_F15  , KC_F16  , KC_F17 , KC_F18 , KC_F19 , KC_F20 ,
   _______ , KC_LEFT , KC_DOWN, KC_RGHT, KC_HOME, KC_END ,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, KC_GRV,
   _______ , KC_TILD , KC_ASTR, KC_NUBS, KC_PMNS, KC_UNDS, KC_LCBR, KC_RCBR, KC_UNDS, KC_PMNS, KC_NUBS, KC_ASTR, KC_BSLS, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
