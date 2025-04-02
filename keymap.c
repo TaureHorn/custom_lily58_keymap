48c79a68aaca14ed818bdab11007028dad884393
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-04-02 12:45:18 +0100

    swapped alt and single quotes

--- keymap.c
+++ keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, _______,
   KC_ESC,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
-  KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN,     KC_RPRN,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LALT,
+  KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_LALT,
+  KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN,     KC_RPRN,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_QUOT,
                             _______,  KC_LGUI,  MO(_L),  KC_SPC,      KC_ENT,  MO(_R), KC_BSPC, _______ 
 ),
 /* _L | lower
